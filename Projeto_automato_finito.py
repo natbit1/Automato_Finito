c29fcaf77c2abd062437799837af54df3a2f8850
natbit1 <natanbittencourt@outlook.com>
2024-12-11 19:57:29 -0300

    Update Projeto_automato_finito.py

--- Projeto_automato_finito.py
+++ Projeto_automato_finito.py
@@ -1,7 +1,17 @@
 #PROJETO AUTÔMATO FINITO
 
+from tkinter import *
+
 class Interface:
 
-    def __init__(self) -> None:
-        pass
-    #fgfgfgfgfgfg
+    def __init__(self, interface):
+        
+        self.interface = interface
+        self.janela = Tk()
+        #ghghghghgh
+
+    #def ConfigInterface(self):
+
+    def Widgets(self):
+
+        frame_input = Frame(self.janela, bd=4, bg=self.cor_ctrle_frames, highlightbackground=self.cor_borda_frames, highlightthickness=self.larg_borda_frames)
