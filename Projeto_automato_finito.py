3f75d71a7719482bcc39efd9f00c9919862c6e3b
natbit1 <natanbittencourt@outlook.com>
2024-12-11 22:51:17 -0300

    Update Projeto_automato_finito.py

--- Projeto_automato_finito.py
+++ Projeto_automato_finito.py
@@ -31,16 +31,19 @@ class Interface:
 
         self.janela.geometry("+{}+{}".format(ajust_hor_tela, 0))
 
-        self.cor_ctrle_frames = 'yellow'
-        self.cor_borda_frames = 'red'
+        self.cor_ctrle_frames = '#424242'
+        self.cor_borda_frames = '#1c1e3d'
         self.larg_borda_frames = 1.0
+        self.marg_frames_x = 0.01 #Margem horizontal do frames
+        self.marg_frames_y = 0.01 #Margem dos frames superior e inferior em relação à borda
+        self.alt_frame_input = 0.060 #Altura do frame dos inputs
 
     def Widgets(self):
 
         self.ConfigInterface()
 
         frame_input = Frame(self.janela, bd=4, bg=self.cor_ctrle_frames, highlightbackground=self.cor_borda_frames, highlightthickness=self.larg_borda_frames)
-        frame_input.place(relx = 0.0, rely = 0.0, relwidth= 0.8, relheight=0.5)
+        frame_input.place(relx = self.marg_frames_x, rely = self.marg_frames_y, relwidth = 1 - 2*self.marg_frames_x, relheight = self.alt_frame_input)
 
         self.janela.mainloop() 
 
