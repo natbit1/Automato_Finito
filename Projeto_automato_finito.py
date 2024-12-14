164a32cd367f019d421969413b93d83da33f8b81
natbit1 <natanbittencourt@outlook.com>
2024-12-13 22:59:01 -0300

    Update Projeto_automato_finito.py

--- Projeto_automato_finito.py
+++ Projeto_automato_finito.py
@@ -1,6 +1,7 @@
 #PROJETO AUTÔMATO FINITO
 
 from tkinter import *
+import tkinter as tk
 
 class Interface:
 
@@ -37,62 +38,76 @@ class Interface:
         self.cor_labels = 'white'
         self.tam_labels = 10
 
+        self.font_entries = 'arial'
+        self.tam_font_entries = 10
+
         self.larg_bd_tab = 1
         self.cor_bd_tab = 'white'
         self.lrel_cel_tab = 0.06
         self.tam_lbl_tab = 8
 
-    def Widgets(self):
-
-        num_ests = 5
+    def Widgets_Input_1(self):
 
         self.ConfigJanela()
 
-        frame_input = Frame(self.janela, bg=self.cor_frames, highlightbackground=self.cor_bd_frames, highlightthickness=self.larg_bd_frames)
-        frame_input.place(relx=0, rely=0, relwidth=0.3, relheight=1)
+        #Frame do input
 
-        alt_frame_input = self.lrel_cel_tab*(num_ests + 1)*frame_input.winfo_screenheight()/frame_input.winfo_screenwidth()
+        self.frame_input_1 = Frame(self.janela, bg=self.cor_frames, highlightbackground=self.cor_bd_frames, highlightthickness=self.larg_bd_frames)
+        self.frame_input_1.place(relx=0, rely=0, relwidth=0.3, relheight=0.07)
 
-        frame_tab = Frame(frame_input, bg=self.cor_frames, highlightbackground=self.cor_bd_tab, highlightthickness=self.larg_bd_tab)
-        frame_tab.place(relx=(1-self.lrel_cel_tab*(num_ests + 1))/2, rely=0.15, relwidth=self.lrel_cel_tab*(num_ests + 1), relheight=alt_frame_input)
+        self.frame_input_2_ant = Frame(self.janela, bg=self.cor_frames, highlightbackground=self.cor_bd_frames, highlightthickness=self.larg_bd_frames)
+        self.frame_input_2_ant.place(relx=0, rely=0.072, relwidth=0.3, relheight=0.928)
 
         frame_diag = Frame(self.janela, bg=self.cor_frames, highlightbackground=self.cor_bd_frames, highlightthickness=self.larg_bd_frames)
         frame_diag.place(relx=0.302, rely=0, relwidth=0.698, relheight=1)
 
-        lbl_alfa = Label(frame_input, text="Alfabeto", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
-        lbl_alfa.place(relx=0.04, rely=0.02)
+        #Entradas de dados
 
-        lbl_num_ests = Label(frame_input, text='Número de estados', bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
-        lbl_num_ests.place(relx=0.52, rely=0.02)
+        entry_var1 = tk.StringVar()
+        entry_var1.trace_add("write", self.Input1)
 
-        lbl_func = Label(frame_input, text="Função de transição", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
-        lbl_func.place(relx=0.34, rely=0.1)
+        self.entry_alfa = Entry(self.frame_input_1, textvariable=entry_var1, font=(self.font_entries, self.tam_font_entries))
+        self.entry_alfa.place(relx=0.2, rely=0.25, relwidth=0.27, relheight=0.5)
 
-        rely_lbl_ests_aceit = alt_frame_input + 0.2
+        entry_var2 = tk.StringVar()
+        entry_var2.trace_add("write", self.Input2)
 
-        lbl_ests_aceit = Label(frame_input, text="Selecionar estados de aceitação", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
-        lbl_ests_aceit.place(relx=0.04, rely=rely_lbl_ests_aceit)
+        self.entry_num_ests = Entry(self.frame_input_1, textvariable=entry_var2, font=(self.font_entries, self.tam_font_entries))
+        self.entry_num_ests.place(relx=0.85, rely=0.25, relwidth=0.08, relheight=0.5)
 
-        lbl_ests_aceit = Label(frame_input, text="Selecionar estado inicial", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
-        lbl_ests_aceit.place(relx=0.04, rely=rely_lbl_ests_aceit+0.1)
+        lbl_alfa = Label(self.frame_input_1, text="Alfabeto", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
+        lbl_alfa.place(relx=0.04, rely=0.25)
 
-        lbl_cadeia = Label(frame_input, text="Cadeia de caracteres", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
-        lbl_cadeia.place(relx=0.03, rely=0.9)
-
-        entry_alfa = Entry(frame_input)
-        entry_alfa.place(relx=0.2, rely=0.022, relwidth=0.25, relheight=0.03)
-
-        entry_num_ests = Entry(frame_input)
-        entry_num_ests.place(relx=0.85, rely=0.022, relwidth=0.08, relheight=0.03)
-
-        entry_cadeia = Entry(frame_input)
-        entry_cadeia.place(relx=0.4, rely=0.904, relwidth=0.25, relheight=0.03)
+        lbl_num_ests = Label(self.frame_input_1, text='Número de estados', bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
+        lbl_num_ests.place(relx=0.52, rely=0.25)
         
-        for i in range(num_ests+1):
+        self.frame_tab_ant = Frame(self.frame_input_1, bg=self.cor_frames, highlightbackground=self.cor_bd_tab)
+        self.frame_tab_ant.place(relx=0, rely=0.102, relwidth=0.1, relheight=0.1)
+
+        #Tabela
+
+        self.janela.mainloop()
+
+    def Widgets_Input_2(self):
+
+        self.frame_input_2 = Frame(self.janela, bg=self.cor_frames, highlightbackground=self.cor_bd_frames, highlightthickness=self.larg_bd_frames)
+        self.frame_input_2.place(relx=0, rely=0.072, relwidth=0.3, relheight=0.928)
+
+        lbl_func = Label(self.frame_input_2, text="Função de transição", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
+        lbl_func.place(relx=0.34, rely=0.04)
+
+        alt_frame_tab = self.lrel_cel_tab*(self.num_ests + 1)*self.frame_input_2.winfo_screenheight()/self.frame_input_2.winfo_screenwidth()
+
+        frame_tab = Frame(self.frame_input_2, bg=self.cor_frames, highlightbackground=self.cor_bd_tab, highlightthickness=self.larg_bd_tab)
+        frame_tab.place(relx=(1-self.lrel_cel_tab*(self.num_ests + 1))/2, rely=0.09, relwidth=self.lrel_cel_tab*(self.num_ests + 1), relheight=alt_frame_tab)
+
+        self.frame_input_2_ant = self.frame_input_2
+
+        for i in range(self.num_ests+1):
 
             frame_tab.rowconfigure(i, weight=1)
 
-            for j in range(num_ests+1):
+            for j in range(self.num_ests+1):
 
                 frame_tab.columnconfigure(j, weight=1) 
                     
@@ -113,29 +128,56 @@ class Interface:
 
                 else:
 
-                    entry_func = Entry(frame_tab, width=0)
-                    entry_func.grid(row=i, column=j, sticky=NSEW, padx=0.5, pady=0.5)
+                    self.entry_func = Entry(frame_tab, width=0, font=(self.font_entries, self.tam_font_entries))
+                    self.entry_func.grid(row=i, column=j, sticky=NSEW, padx=0.5, pady=0.5)
 
-        rely_cb_est = rely_lbl_ests_aceit+0.06
-        marg_cb_frame = 0.85/num_ests
-        marg_rel_cb = (1 - 2*marg_cb_frame)/(num_ests-1)
+        rely_lbl_ests_aceit = alt_frame_tab + 0.13
 
-        for i in range(num_ests):
+        lbl_ests_aceit = Label(self.frame_input_2, text="Selecionar estados de aceitação", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
+        lbl_ests_aceit.place(relx=0.04, rely=rely_lbl_ests_aceit)
 
-            cb_est_1 = Checkbutton(frame_input, text='q'+str(i), bg=self.cor_frames, fg='white', selectcolor="black")
+        lbl_ests_aceit = Label(self.frame_input_2, text="Selecionar estado inicial", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
+        lbl_ests_aceit.place(relx=0.04, rely=rely_lbl_ests_aceit+0.1)
+
+        lbl_cadeia = Label(self.frame_input_2, text="Cadeia de caracteres", bg=self.cor_frames, fg=self.cor_labels, font=(self.font_labels, self.tam_labels, 'bold'))
+        lbl_cadeia.place(relx=0.03, rely=0.9)
+
+        self.entry_cadeia = Entry(self.frame_input_2, font=(self.font_entries, self.tam_font_entries))
+        self.entry_cadeia.place(relx=0.4, rely=0.904, relwidth=0.25, relheight=0.03)
+
+        rely_cb_est = rely_lbl_ests_aceit+0.065
+        marg_cb_frame = 0.85/self.num_ests
+        marg_rel_cb = (1 - 2*marg_cb_frame)/(self.num_ests-1)
+
+        for i in range(self.num_ests):
+
+            cb_est_1 = Checkbutton(self.frame_input_2, text='q'+str(i), bg=self.cor_frames, fg='white', selectcolor="black")
             cb_est_1.place(relx=marg_cb_frame + i*marg_rel_cb, rely=rely_cb_est, anchor=CENTER)
 
             marg_cb_est_2 = rely_cb_est+0.1
 
-            cb_est_2 = Checkbutton(frame_input, text='q'+str(i), bg=self.cor_frames, fg='white', selectcolor="black")
+            cb_est_2 = Checkbutton(self.frame_input_2, text='q'+str(i), bg=self.cor_frames, fg='white', selectcolor="black")
             cb_est_2.place(relx=marg_cb_frame + i*marg_rel_cb, rely=marg_cb_est_2, anchor=CENTER)
 
-        button_input = Button(frame_input, text="Gerar autômato")
+        button_input = Button(self.frame_input_2, text="Gerar autômato")
         button_input.place(relx=0.375, rely=marg_cb_est_2 + 0.06, relwidth=0.25)
 
-        button_input = Button(frame_input, text="Processar")
+        button_input = Button(self.frame_input_2, text="Processar")
         button_input.place(relx=0.7, rely=0.900, relwidth=0.25)
 
-        self.janela.mainloop() 
+    def Input1(self,*args):
 
-interface = Interface('USUARIO').Widgets()
+        self.alfa = self.entry_alfa.get()
+
+        print(self.alfa)
+
+    def Input2(self,*args):
+
+        for widget in self.frame_input_2_ant.winfo_children(): widget.destroy()
+
+        self.num_ests = int(self.entry_num_ests.get())
+
+        self.Widgets_Input_2()
+
+
+interface = Interface('USUARIO').Widgets_Input_1()
